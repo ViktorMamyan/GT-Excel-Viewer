6febb7db3baccc8ca9a8fd299f918c031faaa021
ViktorMamyan <viktormamyan@hotmail.com>
2018-10-08 16:52:20 +0400

    Project Added

---
+++ GT Excel Viewer/MainForm.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace GT_Excel_Viewer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.formAssistant1 = new DevExpress.XtraBars.FormAssistant();
+            this.defaultLookAndFeel1 = new DevExpress.LookAndFeel.DefaultLookAndFeel(this.components);
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnLoadData = new DevExpress.XtraEditors.SimpleButton();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.GridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.GridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.GridControl2 = new DevExpress.XtraGrid.GridControl();
+            this.GridView2 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.GridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GridView1)).BeginInit();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.GridControl2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // defaultLookAndFeel1
+            // 
+            this.defaultLookAndFeel1.LookAndFeel.SkinName = "Office 2016 Colorful";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 200F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 300F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel3, 2, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(862, 401);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnLoadData);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(194, 395);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnLoadData
+            // 
+            this.btnLoadData.AllowFocus = false;
+            this.btnLoadData.Appearance.Font = new System.Drawing.Font("Tahoma", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnLoadData.Appearance.Options.UseFont = true;
+            this.btnLoadData.Appearance.Options.UseTextOptions = true;
+            this.btnLoadData.Appearance.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Near;
+            this.btnLoadData.ButtonStyle = DevExpress.XtraEditors.Controls.BorderStyles.UltraFlat;
+            this.btnLoadData.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLoadData.Image = ((System.Drawing.Image)(resources.GetObject("btnLoadData.Image")));
+            this.btnLoadData.Location = new System.Drawing.Point(9, 97);
+            this.btnLoadData.Name = "btnLoadData";
+            this.btnLoadData.Size = new System.Drawing.Size(170, 37);
+            this.btnLoadData.TabIndex = 6;
+            this.btnLoadData.Text = "Բեռնել Տվյալները";
+            this.btnLoadData.Click += new System.EventHandler(this.btnLoadData_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(9, 9);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(83, 73);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.GridControl1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(203, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(294, 395);
+            this.panel2.TabIndex = 1;
+            // 
+            // GridControl1
+            // 
+            this.GridControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.GridControl1.EmbeddedNavigator.Margin = new System.Windows.Forms.Padding(4);
+            this.GridControl1.Location = new System.Drawing.Point(0, 0);
+            this.GridControl1.MainView = this.GridView1;
+            this.GridControl1.Margin = new System.Windows.Forms.Padding(4);
+            this.GridControl1.Name = "GridControl1";
+            this.GridControl1.Size = new System.Drawing.Size(294, 395);
+            this.GridControl1.TabIndex = 2;
+            this.GridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.GridView1});
+            // 
+            // GridView1
+            // 
+            this.GridView1.GridControl = this.GridControl1;
+            this.GridView1.Name = "GridView1";
+            this.GridView1.OptionsBehavior.AllowAddRows = DevExpress.Utils.DefaultBoolean.False;
+            this.GridView1.OptionsBehavior.AllowDeleteRows = DevExpress.Utils.DefaultBoolean.False;
+            this.GridView1.OptionsBehavior.Editable = false;
+            this.GridView1.OptionsBehavior.ReadOnly = true;
+            this.GridView1.OptionsCustomization.AllowColumnMoving = false;
+            this.GridView1.OptionsCustomization.AllowGroup = false;
+            this.GridView1.OptionsSelection.EnableAppearanceFocusedCell = false;
+            this.GridView1.OptionsSelection.EnableAppearanceFocusedRow = false;
+            this.GridView1.OptionsSelection.MultiSelect = true;
+            this.GridView1.OptionsView.EnableAppearanceOddRow = true;
+            this.GridView1.OptionsView.ShowAutoFilterRow = true;
+            this.GridView1.OptionsView.ShowGroupPanel = false;
+            this.GridView1.DoubleClick += new System.EventHandler(this.GridView1_DoubleClick);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.GridControl2);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(503, 3);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(406, 395);
+            this.panel3.TabIndex = 2;
+            // 
+            // GridControl2
+            // 
+            this.GridControl2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.GridControl2.EmbeddedNavigator.Margin = new System.Windows.Forms.Padding(4);
+            this.GridControl2.Location = new System.Drawing.Point(0, 0);
+            this.GridControl2.MainView = this.GridView2;
+            this.GridControl2.Margin = new System.Windows.Forms.Padding(4);
+            this.GridControl2.Name = "GridControl2";
+            this.GridControl2.Size = new System.Drawing.Size(406, 395);
+            this.GridControl2.TabIndex = 2;
+            this.GridControl2.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.GridView2});
+            // 
+            // GridView2
+            // 
+            this.GridView2.GridControl = this.GridControl2;
+            this.GridView2.Name = "GridView2";
+            this.GridView2.OptionsBehavior.AllowAddRows = DevExpress.Utils.DefaultBoolean.False;
+            this.GridView2.OptionsBehavior.AllowDeleteRows = DevExpress.Utils.DefaultBoolean.False;
+            this.GridView2.OptionsBehavior.Editable = false;
+            this.GridView2.OptionsBehavior.ReadOnly = true;
+            this.GridView2.OptionsCustomization.AllowColumnMoving = false;
+            this.GridView2.OptionsCustomization.AllowGroup = false;
+            this.GridView2.OptionsSelection.EnableAppearanceFocusedCell = false;
+            this.GridView2.OptionsSelection.EnableAppearanceFocusedRow = false;
+            this.GridView2.OptionsSelection.MultiSelect = true;
+            this.GridView2.OptionsView.EnableAppearanceOddRow = true;
+            this.GridView2.OptionsView.ShowAutoFilterRow = true;
+            this.GridView2.OptionsView.ShowGroupPanel = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(862, 401);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "MainForm";
+            this.Text = "GT Excel Viewer";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.GridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GridView1)).EndInit();
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.GridControl2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GridView2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraBars.FormAssistant formAssistant1;
+        private DevExpress.LookAndFeel.DefaultLookAndFeel defaultLookAndFeel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private DevExpress.XtraEditors.SimpleButton btnLoadData;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        internal DevExpress.XtraGrid.GridControl GridControl1;
+        internal DevExpress.XtraGrid.Views.Grid.GridView GridView1;
+        internal DevExpress.XtraGrid.GridControl GridControl2;
+        internal DevExpress.XtraGrid.Views.Grid.GridView GridView2;
+    }
+}
+
---
+++ GT Excel Viewer/MainForm.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Data.OleDb;
+
+namespace GT_Excel_Viewer
+{
+
+    public partial class MainForm : DevExpress.XtraEditors.XtraForm
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        List<ExcelInfo> EI = new List<ExcelInfo>();
+        string FileName = string.Empty;
+
+        private void btnLoadData_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FileName = string.Empty;
+                EI.Clear();
+                
+                ResetAllGrids();
+
+                GetFilePath();
+
+                if (!String.IsNullOrEmpty(FileName))
+                {
+                    LoadExcelFile();    
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void GetFilePath()
+        {
+            OpenFileDialog fDialog = new OpenFileDialog()
+            {
+                Filter = "",
+                Multiselect = false,
+            };
+
+            fDialog.ShowDialog();
+
+            FileName = fDialog.FileName;
+        }
+
+        private void ResetResultGrid()
+        {
+            GridControl1.BeginUpdate();
+            GridControl1.DataSource = null;
+            GridView1.Columns.Clear();
+            GridControl1.EndUpdate();
+        }
+
+        private void ResetAllGrids()
+        {
+            ResetResultGrid();
+
+            GridControl2.BeginUpdate();
+            GridControl2.DataSource = null;
+            GridView2.Columns.Clear();
+            GridControl2.EndUpdate();
+        }
+
+        private void LoadExcelFile()
+        {
+            if (String.IsNullOrEmpty(FileName)) throw new Exception("Select file");
+
+            OleDbConnection connExcel = new OleDbConnection();
+            OleDbCommand cmdExcel = new OleDbCommand();
+            OleDbDataAdapter oda = new OleDbDataAdapter();
+
+            connExcel = new System.Data.OleDb.OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0; Data Source=" + FileName + ";Extended Properties=Excel 12.0;");
+            cmdExcel.Connection = connExcel;
+            connExcel.Open();
+            System.Data.DataTable dt = connExcel.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+
+            if (dt == null) throw new Exception("File read error");
+
+            foreach (DataRow row in dt.Rows)
+            {
+                string t = row["TABLE_NAME"].ToString();
+                EI.Add(new ExcelInfo { ID = t, Sheet = CleanUpSheetName(t) });
+            }
+
+            CleanUpSheetName(EI[0].ID);
+
+            connExcel.Close();
+            
+            GridControl1.BeginUpdate();
+            GridControl1.DataSource = null;
+            GridView1.Columns.Clear();
+
+            GridControl1.DataSource = Ext.ToDataTable(EI);
+
+            GridView1.Columns["ID"].Visible = false;
+
+            GridView1.ClearSelection();
+            GridControl1.EndUpdate();
+        }
+
+        string CleanUpSheetName(string sName)
+        {
+            int StartCharPos = sName.IndexOf("_");
+            int EndCharPos = sName.IndexOf("$");
+            return sName.Substring(StartCharPos + 1, EndCharPos - (StartCharPos + 2)).ToString().Trim();
+        }
+
+        private void GridView1_DoubleClick(object sender, EventArgs e)
+        {
+            DevExpress.XtraGrid.Views.Grid.GridView view = (DevExpress.XtraGrid.Views.Grid.GridView)sender;
+            Point pt = view.GridControl.PointToClient(Control.MousePosition);
+            DoRowDoubleClick(view, pt);
+        }
+
+        private void DoRowDoubleClick(DevExpress.XtraGrid.Views.Grid.GridView view, Point pt)
+        {
+            try
+            {
+                DevExpress.XtraGrid.Views.Grid.ViewInfo.GridHitInfo info = view.CalcHitInfo(pt);
+                if (info.InRow || info.InRowCell)
+                {
+                    if (info.Column == null)
+                    {
+                    }
+                    else
+                    {
+                        if (view.GetRowCellValue(info.RowHandle, "ID") == DBNull.Value) return;
+
+                        string sheet = view.GetRowCellValue(info.RowHandle, "ID").ToString();
+
+                        GetDetils(sheet);
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void GetDetils(string sheet)
+        {
+            if (String.IsNullOrEmpty(FileName)) throw new Exception("Select file");
+
+            GridControl2.BeginUpdate();
+            GridControl2.DataSource = null;
+            GridView2.Columns.Clear();
+
+            OleDbConnection connExcel = new OleDbConnection();
+            OleDbCommand cmdExcel = new OleDbCommand();
+            OleDbDataAdapter oda = new OleDbDataAdapter();
+            System.Data.DataSet ds = new System.Data.DataSet();
+
+            connExcel = new System.Data.OleDb.OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0; Data Source=" + FileName + ";Extended Properties=Excel 12.0;");
+
+            oda = new System.Data.OleDb.OleDbDataAdapter("select * FROM [" + sheet + "]", connExcel);
+            oda.TableMappings.Add("Table", "Excel Data");
+            oda.Fill(ds);
+
+            connExcel.Close();
+
+            GridControl2.DataSource = ds.Tables[0];
+
+            GridView2.ClearSelection();
+            GridControl2.EndUpdate();
+        }
+    
+    }
+
+    public class ExcelInfo
+    {
+        public string ID { get; set; }
+        public string Sheet { get; set; }
+    }
+
+    public static class Ext
+    {
+
+        internal static DataTable ToDataTable<T>(this IList<T> data)
+        {
+            PropertyDescriptorCollection properties = TypeDescriptor.GetProperties(typeof(T));
+            DataTable dt = new DataTable();
+            for (int i = 0; i <= properties.Count - 1; i++)
+            {
+                PropertyDescriptor property = properties[i];
+                //dt.Columns.Add(property.Name, property.PropertyType);
+                dt.Columns.Add(property.Name, Nullable.GetUnderlyingType(property.PropertyType) ?? property.PropertyType);
+            }
+            object[] values = new object[properties.Count - 1 + 1];
+            foreach (T item in data)
+            {
+                for (int i = 0; i <= values.Length - 1; i++)
+                    values[i] = properties[i].GetValue(item);
+                dt.Rows.Add(values);
+            }
+            return dt;
+        }
+
+    }
+
+}
